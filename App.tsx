c0e1c84e3fa1052655e5a818110e89b510499153
Trong Minh Do <156078684+trongminhSE172130@users.noreply.github.com>
2025-02-06 20:32:01 +0700

    update

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app! SWD392-MB</Text>
+      <Text>Open up App.tsx to start working on your app! SWD392-MBb</Text>
       <StatusBar style="auto" />
     </View>
   );
